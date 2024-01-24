49956b9c45b13cea9c8934d5d09776009c17e35b
Luis Gutzeit <Luigutzeit@web.de>
2024-01-24 10:59:41 +0100

    small improvements

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #define alarmLED D4
 #define empfangLED D3
 
-//VentMessages
+// VentMessages
 #define V_AN 0xC08
 #define V_AUS 0xC20
 #define V_OFF 0xC10
@@ -17,7 +17,7 @@
 #define V_MED 0xC04
 #define V_HIGH 0xC01
 
-//RC Messages
+// RC Messages
 #define R_1AN 2394740224
 #define R_1AUS 2176636416
 #define R_2AN 2797393408
@@ -34,109 +34,117 @@ boolean alarmAktiv = false;
 uint8_t SteckerTimer = 0;
 boolean GarageZu = true;
 
-IRsend irsend(IrLED);  // Set the GPIO to be used to sending the message.
+IRsend irsend(IrLED); // Set the GPIO to be used to sending the message.
 RCSwitch RC = RCSwitch();
 
-void VentMessage(int code){
+void VentMessage(int code)
+{
   irsend.sendSymphony(0xC00);
   irsend.sendSymphony(0xC7F);
   irsend.sendSymphony(code);
 }
 
-void Alarm(){
+void Alarm()
+{
   LichtAn = true;
   LichtTimer = millis();
   alarmAktiv = true;
   VentMessage(V_AN);
-  RC.send(R_1AN,32);
-  RC.send(R_2AN,32);
+  RC.send(R_1AN, 32);
+  RC.send(R_2AN, 32);
   Serial.print("ALARM\n");
 }
 
-void Quittiert(){
+void Quittiert()
+{
   alarmAktiv = false;
   Serial.print("ENDE\n");
 }
 
 void LichtAus()
 {
-    LichtTimer = 0;
-    LichtAn = false;
-    RC.send(R_1AUS,32);
-    RC.send(R_2AUS,32);
-    VentMessage(V_AUS);
+  LichtTimer = 0;
+  LichtAn = false;
+  RC.send(R_1AUS, 32);
+  RC.send(R_2AUS, 32);
+  VentMessage(V_AUS);
 }
 
 void manageSerial()
 {
-  if(Serial.available()){
+  if (Serial.available())
+  {
     String rec = Serial.readStringUntil('\n');
-    digitalWrite(empfangLED,true);  
-    if(rec == "OFF")
+    digitalWrite(empfangLED, true);
+    if (rec == "OFF")
       VentMessage(V_OFF);
-    if(rec == "LOW")
+    if (rec == "LOW")
       VentMessage(V_LOW);
-    if(rec == "MED")
+    if (rec == "MED")
       VentMessage(V_MED);
-    if(rec == "HI")
+    if (rec == "HI")
       VentMessage(V_HIGH);
-    if(rec == "ON")
+    if (rec == "ON")
       VentMessage(V_AN);
-    if(rec == "LIGHTOFF")
+    if (rec == "LIGHTOFF")
       LichtAus();
-    if(rec == "TEST")
+    if (rec == "TEST")
       Alarm();
-    if(rec == "GarageOffen")
+    if (rec == "GarageOffen")
       GarageZu = false;
-    if(rec == "GarageZu")
-      GarageZu = true;   
-    }
-    digitalWrite(empfangLED,false);
-    Serial.flush();
+    if (rec == "GarageZu")
+      GarageZu = true;
+  }
+  digitalWrite(empfangLED, false);
+  Serial.flush();
 }
 
-//Hier beginnt das eigentliche Programm
+// Hier beginnt das eigentliche Programm
 
-void setup() {
-  //WiFi nicht benötigt -> Aus
+void setup()
+{
+  // WiFi nicht benötigt -> Aus
   WiFi.persistent(false);
   WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);
   WiFi.forceSleepBegin();
 
-  //RC einrichten
+  // RC einrichten
   RC.enableTransmit(rSender);
   RC.setProtocol(2);
   RC.setPulseLength(714);
   RC.setRepeatTransmit(3);
 
   irsend.begin();
-  pinMode(melderPin,INPUT_PULLUP);
-  pinMode(alarmLED,OUTPUT);
-  pinMode(empfangLED,OUTPUT);
+  pinMode(melderPin, INPUT_PULLUP);
+  pinMode(alarmLED, OUTPUT);
+  pinMode(empfangLED, OUTPUT);
 
-  //Serielle Schnittstelle einrichten
-  Serial.begin(115200,SERIAL_8E1);
+  // Serielle Schnittstelle einrichten
+  Serial.begin(115200, SERIAL_8E1);
 }
 
-void loop() {
-  if(millis() - LichtTimer >= LichtAusZeit && LichtAn){
+void loop()
+{
+  if (millis() - LichtTimer >= LichtAusZeit && LichtAn)
+  {
     LichtAus();
   }
 
-  if(digitalRead(melderPin) == LOW && !alarmAktiv)
+  if (digitalRead(melderPin) == LOW && !alarmAktiv)
   {
     delay(10);
-    if(digitalRead(melderPin) == LOW && !alarmAktiv)
+    if (digitalRead(melderPin) == LOW && !alarmAktiv)
       Alarm();
   }
 
-  if(digitalRead(melderPin) == HIGH && alarmAktiv)
+  if (digitalRead(melderPin) == HIGH && alarmAktiv)
   {
     delay(10);
-    if(digitalRead(melderPin) == HIGH && alarmAktiv)
+    if (digitalRead(melderPin) == HIGH && alarmAktiv)
       Quittiert();
   }
-  
-  digitalWrite(alarmLED,!GarageZu);
+
+  digitalWrite(alarmLED, !GarageZu);
   manageSerial();
 }
